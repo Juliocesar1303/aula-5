3179e14606580a1e3b05c9ea8713827e32060ab9
Juliocesar1303 <142456148+Juliocesar1303@users.noreply.github.com>
2023-08-21 21:45:02 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+/*
+* Julio cesar
+* Aprendedo mais
+*/ console.log("Olá mundo");
+//prompt("Digite seu nome");
+
+//Variaveis
+const variaveiA =100;
+const variaveiB = variaveiA + 50;
+console.log(variaveiB);
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+const nomeExercicio = "Julio cesar";
+const idadeExercicio = 32;
+
+console.log("Olá!", "Meu nome é ", nomeExercicio, "e eu tenho", idadeExercicio, "anos");
+
+const primeiro = "Julio";
+const sobrenome = "Cesar";
+const idade = 32;
+const estudante = "sou estudante";
+
+console.log("olá!", "meu nome", primeiro, sobrenome, "tenho", idade, "anos e eu", estudante);
+
+const samuka = "samuka venenozo"
+console.log("meu cunhado é o", samuka)
+;
+
+const Ana = "Ana s2"
+console.log("eu amo muito vc", Ana,);
